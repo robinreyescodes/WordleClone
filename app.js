a11ac3aebf5bf987a4ff5c84d05791a2fd9515e2
Robin Reyes <robinreyes@Robins-MacBook-Pro.local>
2022-02-08 18:38:18 -0800

    typing functionality added

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 "use strict";
 
 const keys = document.querySelectorAll(".kb-tiles");
+const tiles = document.querySelectorAll(".tile1");
 
-document.addEventListener("keydown", (e) => {
-  const hi = e.key.toUpperCase();
-  keys.forEach((key) => {
-    if (key.textContent === hi) console.log(key.textContent);
-  });
+let string = "";
+let i = 0;
+
+document.addEventListener("keypress", (e) => {
+  if (i !== 0 && (i + 1) % 5 === 0) {
+    if (e.key === "Enter") {
+      string += tiles[i].textContent.toLowerCase();
+      console.log(string);
+      i++;
+      string = "";
+    }
+    if (e.key !== "Enter") {
+      tiles[i].textContent = e.key.toUpperCase();
+    }
+  } else if (i < tiles.length && e.key !== "Enter") {
+    string += e.key;
+    tiles[i].textContent = e.key.toUpperCase();
+    i++;
+  }
 });
